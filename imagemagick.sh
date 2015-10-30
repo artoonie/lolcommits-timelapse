c5b640fd530d0c07c822d1eeeebeb103b2fbfb01
Armin Samii <asamii@adobe.com>
2015-10-30 13:50:53 -0700

    Bugfix: included wrong days in one-digit days
    Days x \in [1,9] would include 1x and 2x as well.
    For example, Oct 5 would include the 15th and 25th.
    
    Also added a line to make it into a movie at the end.

--- imagemagick.sh
+++ imagemagick.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 
 allFiles=$(ls -lRtTr .lolcommits/*/*.jpg ) # T: always print year, t: sort by date, r: reverse
-# dateList=$(echo "$allFiles" | awk '{printf("%d %s %d : %s\n", $9, $6, $7, $10)}' | uniq)
-dateList=$(echo "$allFiles" | awk '{printf("%s.*%d.*%d\n", $6, $7, $9)}' | uniq)
+dateList=$(echo "$allFiles" | awk '{printf("%s\\s*%d.*%d\n", $6, $7, $9)}' | uniq)
 
 mkdir -p results results/montages results/annotated results/intermediates
 for date in $dateList;
 do
-    filesOnThisDate=$(echo "$allFiles" | grep $date | awk '{print $10}')
-    # echo "On date $date, files are $filesOnThisDate"
-    dateWithoutDot=$(echo $date | tr -d '.' | tr -d '*')
-    dateWithSpaces=$(echo $date | tr -d '.' | tr '*' ' ')
-    filename=${dateWithoutDot}.png
+    filesOnThisDate=$(echo "$allFiles" | grep -p "$date" | awk '{print $10}')
+    dateWithoutRegex=$(echo $date | tr -d '.\\s *') # Luckly, no month has lowercase s
+    dateWithSpaces=$(echo $date | tr -d '.\\s ' | tr '*' ' ')
+
     echo $dateWithSpaces
+
+    filename=${dateWithoutRegex}.png
     montage -geometry 640x480 -background Black $filesOnThisDate results/montages/$filename
-    # convert montages/$filename \
-    #         -gravity Center -background Black -fill Gray \
-    #         -pointsize 70 \
-    #         -resize 800x450\! -size 800x450\! \
-    #         label:"$dateWithSpaces" -append  annotated/$filename
+
     convert results/montages/$filename \
         -resize 800x450 -size 800x450\! \
         -gravity north  -extent 800x450 \
@@ -29,3 +25,5 @@ do
         -stroke  none   -fill white     -annotate 0 "$dateWithSpaces" \
         results/annotated/$filename
 done
+
+convert `find annotated -type f -name "*.png" -print0 | xargs -0 ls -tlr | awk '{print $9}'` movie.mp4
